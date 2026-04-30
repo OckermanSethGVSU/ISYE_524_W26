0f403f3fc07b82827bde183d44934f1425472023
Seth Ockerman <sockerman@wisc.edu>
2026-04-29 21:21:18 -0500

    add the gen of the slighly bigger example

--- gen_data.sh
+++ gen_data.sh
@@ -16,3 +16,6 @@ for i in 0 1 2 3; do
         --complexity "$i" \
         --visualization-layout separate
 done
+
+rm -fr bigger_level3data
+python3 generate_supply_chain_data.py     --output-dir bigger_level3data     --seed 2     --customers 4     --local-shops 4     --regional-warehouses 2     --central-depots 1     --specialized-shops 4     --components 40    --complexity 3     --visualize     --visualization-layout separate
